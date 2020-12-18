c3c0cd4b360eabc4bf373cebd0b9cfdf35d192e8
xuzhou524 <xuuohz@163.com>
2020-12-18 10:40:38 +0800

    bak

--- Controller/HomeViewController.swift
+++ Controller/HomeViewController.swift
@@ -11,7 +11,15 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor(named: "color_title_gray")
+        self.view.backgroundColor = UIColor(named: "color_theme")
 
+        let view = UIView()
+        view.backgroundColor = UIColor(named: "color_themeTabar")
+        self.view.addSubview(view)
+        view.snp.makeConstraints { (make) in
+            make.left.right.bottom.equalToSuperview()
+            make.height.equalTo(100)
+        }
+        
     }
 }
