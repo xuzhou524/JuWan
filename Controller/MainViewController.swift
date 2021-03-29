bf38de057e15bc1338b00112cc34152b6bb33b0c
Aaron_xu <xuzhou@gozap.com>
2021-03-29 16:11:44 +0800

    清空所有文件

--- Controller/MainViewController.swift
+++ Controller/MainViewController.swift
@@ -6,22 +6,14 @@
 //
 
 import UIKit
-import GameKit
-import GameplayKit
 
-class MainViewController: UIViewController,GKGameCenterControllerDelegate {
+class MainViewController: UIViewController {
     
     var isNoFirst: Bool = true
-
-    let emptyView:EmptyCell = {
-        let view = EmptyCell()
-        return view
-    }()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        authenticateLocalPlayer()
+
     }
     
     override func viewDidLoad() {
@@ -43,104 +35,10 @@ class MainViewController: UIViewController,GKGameCenterControllerDelegate {
     }
     
     func sebViews() {
-        
-        let viewWatermelon = HomeCardView()
-        viewWatermelon.layer.cornerRadius = 10
-        viewWatermelon.layer.masksToBounds = true
-        viewWatermelon.tipButton.addTarget(self, action: #selector(watermelonTapped), for: .touchUpInside)
-        viewWatermelon.tipButton.setTitle("玩一下", for: .normal)
-        viewWatermelon.frame = CGRect(x: 0, y: 0 , width: kScreenWidth, height: 230)
-        
-        viewWatermelon.bgImageView.image = UIImage(named: "ic_Watermelon")
-        viewWatermelon.titleLabel.text = "我要吃瓜"
-        viewWatermelon.summeryLabel.text = "休闲吃瓜，挑战全球排行"
-        viewWatermelon.rulesLabel.text = "规则：两个体型较小的水果相撞即可合成更大的水果，最终得到想吃的瓜"
-        viewWatermelon.starImageView.image = UIImage(named: "ic_starTwo")?.withRenderingMode(.alwaysTemplate)
-        
-        self.view.addSubview(viewWatermelon)
-        
-        let viewShuHe = HomeCardView()
-        viewShuHe.layer.cornerRadius = 10
-        viewShuHe.layer.masksToBounds = true
-        viewShuHe.tipButton.addTarget(self, action: #selector(startGameButtonTapped), for: .touchUpInside)
-        viewShuHe.tipButton.setTitle("玩一下", for: .normal)
-        viewShuHe.frame = CGRect(x: 0, y: 230, width: kScreenWidth, height: 230)
-        
-        self.view.addSubview(viewShuHe)
-        
-    }
-    
-    func sebWithoutAuthorizationViews() {
-        self.view.addSubview(emptyView)
-        emptyView.snp.makeConstraints { (make) in
-            make.top.left.right.bottom.equalTo(self.view)
-        }
-    }
-    
-    @objc func startGameButtonTapped() {
-        let v = HomeViewController()
-        self.navigationController?.pushViewController(v, animated: true)
-    }
-    
-    @objc func watermelonTapped() {
-        let v = WatermelonViewController()
-        self.navigationController?.pushViewController(v, animated: true)
-    }
-
-    @objc func globalButtonTapped() {
-        let gameCenterVC = GKGameCenterViewController()
-        gameCenterVC.gameCenterDelegate = self
-        self.present(gameCenterVC, animated: true, completion: nil)
+   
     }
     
     @objc func rightBtnClick() {
-        if GameUserInfoConfig.shared.gameId.count > 0 {
-            self.navigationController?.pushViewController(UserViewController(), animated: true)
-        }else{
-            self.navigationController?.pushViewController(HelpViewController(), animated: true)
-        }
-    }
-    
-    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
-        gameCenterViewController.dismiss(animated: true, completion: nil)
-    }
-    
-}
-
-extension MainViewController {
-    func authenticateLocalPlayer(){
-        if isNoFirst {
-            isNoFirst = false
-            self.showloading()
-        }
-        let localPlayer = GKLocalPlayer.local
-        localPlayer.authenticateHandler = {(viewController, error) -> Void in
-            self.hideLoading()
-            if (viewController != nil) {
-                let vc: UIViewController = self.view!.window!.rootViewController!
-                vc.present(viewController!, animated: true) {
-                    self.checkLocalAuthenticated()
-                }
-            }else {
-                self.checkLocalAuthenticated()
-            }
-        }
-    }
-    
-    func checkLocalAuthenticated() {
-        if !GKLocalPlayer.local.isAuthenticated {
-            print("没有授权，无法获取更多信息")
-            GameUserInfoConfig.shared.gameId = ""
-            GameUserInfoConfig.shared.gameName = "聚玩者"
-            sebWithoutAuthorizationViews()
-        }else{
-            //存储玩家信息 - id - name
-            let localPlayer = GKLocalPlayer.local
-            GameUserInfoConfig.shared.gameId = localPlayer.gamePlayerID
-            GameUserInfoConfig.shared.gameName = localPlayer.displayName
-            
-            sebViews()
-
-        }
+        
     }
 }
